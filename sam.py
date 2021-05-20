30daaba6a619e97379046568b2cf03ac26de490e
Simon Biggs <me@simonbiggs.net>
2021-05-20 14:51:12 +1000

    Update sam.py

--- sam.py
+++ sam.py
@@ -10,7 +10,7 @@ class SAM():
 
     def first_step(self, gradients, trainable_variables):
         self.e_ws = []
-        grad_norm = tf.linalg.global_norm(trainable_variables)
+        grad_norm = tf.linalg.global_norm(gradients)
         for i in range(len(trainable_variables)):
             e_w = gradients[i] * self.rho / (grad_norm + 1e-12)
             trainable_variables[i].assign_add(e_w)
