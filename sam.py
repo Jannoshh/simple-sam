cd77d0217128aa4ad3dcea558403b5bca93b5952
Jannes Elstner <jannoshjannes@gmail.com>
2022-03-10 15:11:59 +0100

    updated examples

--- sam.py
+++ sam.py
@@ -2,20 +2,21 @@ import tensorflow as tf
 
 
 class SAM():
-    def __init__(self, base_optimizer, rho=0.05):
+    def __init__(self, base_optimizer, rho=0.05, eps=1e-12):
         assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
         
         self.rho = rho
+        self.eps = eps
         self.base_optimizer = base_optimizer
 
-    def first_step(self, gradients, trainable_variables):
+    def first_step(self, gradients, trainable_vars):
         self.e_ws = []
         grad_norm = tf.linalg.global_norm(gradients)
-        ew_multiplier = rho / (grad_norm + eps)
+        ew_multiplier = self.rho / (grad_norm + self.eps)
         for i in range(len(trainable_vars)):
             e_w = tf.math.multiply(gradients[i], ew_multiplier)
-            trainable_variables[i].assign_add(e_w)
-            e_ws.append(e_w)
+            trainable_vars[i].assign_add(e_w)
+            self.e_ws.append(e_w)
 
     def second_step(self, gradients, trainable_variables):
         for i in range(len(trainable_variables)):
